a0a1ce9f082fe1c422681eb0fac159d1da2444e9
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-04-19 15:08:00 +0000

    Now able to actually collect stats from WebSphere.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/StatsEnabledMBeanResourceComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/StatsEnabledMBeanResourceComponent.java
@@ -1,5 +1,7 @@
 package be.fgov.kszbcss.websphere.rhq;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,27 +23,60 @@ public class StatsEnabledMBeanResourceComponent extends MBeanResourceComponent {
         // We create a new PropertyUtilsBean every time in order to avoid keeping references
         // to class loaders used by EMS (which could create a leak).
         PropertyUtilsBean propUtils = new PropertyUtilsBean();
-        StatsDynaBean stats = null;
+        Object stats = null;
+        Method getStatisticMethod = null;
         for (MeasurementScheduleRequest request : (Set<MeasurementScheduleRequest>)requests) {
             String name = request.getName();
             if (name.startsWith("stats.")) {
                 if (stats == null) {
-                    stats = new StatsDynaBean(bean.getAttribute("stats").getValue());
+                    stats = bean.getAttribute("stats").getValue();
+                    Class<?> statsClass = stats.getClass();
+                    if (log.isDebugEnabled()) {
+                        try {
+                            String[] statsNames = (String[])statsClass.getMethod("getStatisticNames").invoke(stats);
+                            log.debug("Loaded Stats object from MBean. Available statistics: " + Arrays.asList(statsNames));
+                        } catch (Exception ex) {
+                            log.debug("Loaded Stats object from MBean, but unable to get statistic names", ex);
+                        }
+                    }
+                    try {
+                        getStatisticMethod = statsClass.getMethod("getStatistic", String.class);
+                    } catch (NoSuchMethodException ex) {
+                        log.error(ex);
+                    }
                 }
-                String propertyName = name.substring(6);
-                Long value;
-                try {
-                    value = (Long)propUtils.getNestedProperty(stats, propertyName);
-                } catch (Exception ex) {
-                    log.error("Failed to get the " + propertyName + " from the Stats object", ex);
-                    continue;
+                if (getStatisticMethod == null) {
+                    log.error("Unable to retrieve data for " + name + " because of previous failure");
+                } else {
+                    int idx = name.indexOf('.', 6);
+                    String statisticName = name.substring(6, idx);
+                    String propertyName = name.substring(idx+1);
+                    Object statistic;
+                    try {
+                        statistic = getStatisticMethod.invoke(stats, statisticName);
+                    } catch (Exception ex) {
+                        log.error("Unable to retrieve statistic for " + statisticName, ex);
+                        continue;
+                    }
+                    Long value;
+                    try {
+                        value = (Long)propUtils.getNestedProperty(statistic, propertyName);
+                    } catch (Exception ex) {
+                        log.error("Failed to get the " + propertyName + " from the Statistic object for " + statisticName, ex);
+                        continue;
+                    }
+                    if (log.isDebugEnabled()) {
+                        log.debug("Adding measurement for " + name + "; value=" + value);
+                    }
+                    report.addData(new MeasurementDataNumeric(request, new Double(value)));
                 }
-                report.addData(new MeasurementDataNumeric(request, new Double(value)));
             } else {
                 simpleRequests.add(request);
             }
         }
-        super.getValues(report, simpleRequests, bean);
+        if (!simpleRequests.isEmpty()) {
+            super.getValues(report, simpleRequests, bean);
+        }
     }
 
 }
