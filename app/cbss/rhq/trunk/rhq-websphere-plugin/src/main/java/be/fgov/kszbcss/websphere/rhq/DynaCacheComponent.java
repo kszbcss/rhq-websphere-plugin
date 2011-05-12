e62240d962389d4f225d90232182dfbfcb2a6416
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-05-12 13:39:39 +0000

    Ignore WebSphere internal DynaCaches and implemented an availability check.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/DynaCacheComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/DynaCacheComponent.java
@@ -2,6 +2,8 @@ package be.fgov.kszbcss.websphere.rhq;
 
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementDataNumeric;
@@ -15,6 +17,8 @@ import org.rhq.core.pluginapi.operation.OperationFacet;
 import org.rhq.core.pluginapi.operation.OperationResult;
 
 public class DynaCacheComponent implements ResourceComponent<WebSphereServerComponent>, MeasurementFacet, OperationFacet {
+    private static final Log log = LogFactory.getLog(DynaCacheComponent.class);
+    
     private MBean mbean;
     private String instanceName;
 
@@ -24,8 +28,19 @@ public class DynaCacheComponent implements ResourceComponent<WebSphereServerComp
     }
 
     public AvailabilityType getAvailability() {
-        // TODO
-        return AvailabilityType.UP;
+        String[] instanceNames;
+        try {
+            instanceNames = (String[])mbean.invoke("getCacheInstanceNames", new Object[0], new String[0]);
+        } catch (Exception ex) {
+            log.error("Unable to get cache instance names", ex);
+            return AvailabilityType.DOWN;
+        }
+        for (String name : instanceNames) {
+            if (name.equals(instanceName)) {
+                return AvailabilityType.UP;
+            }
+        }
+        return AvailabilityType.DOWN;
     }
 
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/DynaCacheDiscoveryComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/websphere/rhq/DynaCacheDiscoveryComponent.java
@@ -3,17 +3,27 @@ package be.fgov.kszbcss.websphere.rhq;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryContext;
 
 public class DynaCacheDiscoveryComponent implements ResourceDiscoveryComponent<WebSphereServerComponent> {
+    private static final Log log = LogFactory.getLog(DynaCacheDiscoveryComponent.class); 
+    
     public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext<WebSphereServerComponent> context) throws InvalidPluginConfigurationException, Exception {
         Set<DiscoveredResourceDetails> result = new HashSet<DiscoveredResourceDetails>();
         MBean mbean = new MBean(context.getParentResourceComponent().getServer(), Utils.createObjectName("WebSphere:type=DynaCache,*"));
         for (String instanceName : (String[])mbean.invoke("getCacheInstanceNames", new Object[0], new String[0])) {
-            result.add(new DiscoveredResourceDetails(context.getResourceType(), instanceName, instanceName, null, instanceName, null, null));
+            if (instanceName.equals("baseCache") || instanceName.startsWith("ws/")) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Ignoring DynaCache " + instanceName);
+                }
+            } else {
+                result.add(new DiscoveredResourceDetails(context.getResourceType(), instanceName, instanceName, null, instanceName, null, null));
+            }
         }
         return result;
     }
