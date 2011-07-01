6c7f09515000e500f91a9ba7f74b393e259db615
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-07-01 09:13:24 +0000

    Added PMI statistics for SIB queue points.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/sib/SIBLocalizationPointComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/sib/SIBLocalizationPointComponent.java
@@ -1,24 +1,42 @@
 package be.fgov.kszbcss.rhq.websphere.component.sib;
 
+import java.util.Set;
+
 import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.domain.measurement.MeasurementReport;
+import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 
+import be.fgov.kszbcss.rhq.websphere.ManagedServer;
 import be.fgov.kszbcss.rhq.websphere.component.WebSphereServiceComponent;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.MeasurementFacetSupport;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.PMIMeasurementHandler;
 
-public abstract class SIBLocalizationPointComponent extends WebSphereServiceComponent<SIBMessagingEngineComponent> {
+public abstract class SIBLocalizationPointComponent extends WebSphereServiceComponent<SIBMessagingEngineComponent> implements MeasurementFacet {
+    private MeasurementFacetSupport measurementFacetSupport;
+    
     @Override
     protected void start() throws InvalidPluginConfigurationException, Exception {
-        // TODO Auto-generated method stub
-        
+        ManagedServer server = getServer();
+        measurementFacetSupport = new MeasurementFacetSupport(this);
+        measurementFacetSupport.addHandler("stats", new PMIMeasurementHandler(server.getServerMBean(),
+                "SIB Service", "SIB Messaging Engines", getResourceContext().getParentResourceComponent().getName(),
+                "Destinations", getPMIModuleName(), getResourceContext().getResourceKey()));
+    }
+    
+    protected abstract String getPMIModuleName();
+    
+    public AvailabilityType getAvailability() {
+        // TODO
+        return AvailabilityType.UP;
     }
 
-    public AvailabilityType getAvailability() {
-        // TODO Auto-generated method stub
-        return null;
+    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
+        // TODO: we should only invoke this is the messaging engine is active
+        measurementFacetSupport.getValues(report, requests);
     }
 
     public void stop() {
-        // TODO Auto-generated method stub
-        
     }
 }
--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/sib/SIBQueuePointComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/sib/SIBQueuePointComponent.java
@@ -1,5 +1,8 @@
 package be.fgov.kszbcss.rhq.websphere.component.sib;
 
 public class SIBQueuePointComponent extends SIBLocalizationPointComponent {
-
+    @Override
+    protected String getPMIModuleName() {
+        return "Queues";
+    }
 }
