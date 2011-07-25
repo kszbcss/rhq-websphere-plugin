03c61544d3b8661b2913ca239296d8412726d95a
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-07-25 07:52:52 +0000

    Added support to measure the DB2 agent CPU time.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/jdbc/db2/DB2MonitorComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/jdbc/db2/DB2MonitorComponent.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -27,11 +28,19 @@ import be.fgov.kszbcss.rhq.websphere.proxy.AdminOperations;
 public class DB2MonitorComponent implements ResourceComponent<DataSourceComponent>, MeasurementFacet {
     private static final Log log = LogFactory.getLog(DB2MonitorComponent.class);
     
+    private static final Map<String,String> expressions;
+    
+    static {
+        expressions = new HashMap<String,String>();
+        expressions.put("agent_usr_cpu_time", "agent_usr_cpu_time_s*1000+agent_usr_cpu_time_ms/1000");
+    }
+    
     private DataSourceComponent dataSourceComponent;
     private AdminOperations adminOperations;
     private String principal;
     private String credentials;
     private Connection connection;
+    private final Map<String,DB2MetricData> lastValues = new HashMap<String,DB2MetricData>();
     
     public void start(ResourceContext<DataSourceComponent> context) throws InvalidPluginConfigurationException, Exception {
         dataSourceComponent = context.getParentResourceComponent();
@@ -73,17 +82,12 @@ public class DB2MonitorComponent implements ResourceComponent<DataSourceComponen
             connection = DriverManager.getConnection(url, info);
         }
         
-        StringBuilder buffer = new StringBuilder("SELECT ");
-        boolean first = true;
+        StringBuilder buffer = new StringBuilder("SELECT A.AGENT_ID");
         for (MeasurementScheduleRequest request : requests) {
-            if (first) {
-                first = false;
-            } else {
-                buffer.append(", ");
-            }
-            buffer.append("SUM(");
-            buffer.append(request.getName());
-            buffer.append(")");
+            String name = request.getName();
+            buffer.append(", ");
+            String expression = expressions.get(name);
+            buffer.append(expression == null ? name : expression);
         }
         buffer.append(" FROM SYSIBMADM.SNAPAPPL AS A, SYSIBMADM.SNAPAPPL_INFO AS AI WHERE A.AGENT_ID=AI.AGENT_ID AND AI.APPL_NAME=?");
         String sql = buffer.toString();
@@ -92,21 +96,43 @@ public class DB2MonitorComponent implements ResourceComponent<DataSourceComponen
             log.debug("Preparing to execute statement: " + sql);
         }
         
+        DB2MetricData[] newData = new DB2MetricData[requests.size()];
+        for (int i=0; i<newData.length; i++) {
+            newData[i] = new DB2MetricData();
+        }
         PreparedStatement stmt = connection.prepareStatement(sql);
         try {
             stmt.setString(1, applName);
             ResultSet rs = stmt.executeQuery();
             try {
-                rs.next();
-                int index = 1;
-                for (MeasurementScheduleRequest request : requests) {
-                    report.addData(new MeasurementDataNumeric(request, (double)rs.getLong(index++)));
+                while (rs.next()) {
+                    long agentId = rs.getLong(1);
+                    for (int i=0; i<requests.size(); i++) {
+                        newData[i].addValue(agentId, rs.getLong(i+2));
+                    }
                 }
             } finally {
                 rs.close();
             }
         } finally {
             stmt.close();
+        }
+
+        int i = 0;
+        for (MeasurementScheduleRequest request : requests) {
+            String name = request.getName();
+            if (log.isDebugEnabled()) {
+                log.debug("Adding measurement for " + name);
+            }
+            DB2MetricData current = newData[i++];
+            DB2MetricData last = lastValues.get(name);
+            if (last == null) {
+                lastValues.put(name, current);
+            } else {
+                last.update(current);
+                current = last;
+            }
+            report.addData(new MeasurementDataNumeric(request, (double)current.getSum()));
         }
     }
 
