c41d91da67aac3dc1082dae25f56349fd767d624
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-08-05 14:51:10 +0000

    Added a 'Version' trait for J2EE applications.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationComponent.java
@@ -1,20 +1,28 @@
 package be.fgov.kszbcss.rhq.websphere.component.j2ee;
 
+import java.util.Set;
+
 import javax.management.ObjectName;
 
 import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.domain.measurement.MeasurementReport;
+import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
 import org.rhq.core.pluginapi.event.EventContext;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
+import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 
 import be.fgov.kszbcss.rhq.websphere.ManagedServer;
 import be.fgov.kszbcss.rhq.websphere.Utils;
 import be.fgov.kszbcss.rhq.websphere.component.WebSphereServiceComponent;
 import be.fgov.kszbcss.rhq.websphere.component.server.WebSphereServerComponent;
 import be.fgov.kszbcss.rhq.websphere.mbean.MBeanClient;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.JMXAttributeGroupHandler;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.MeasurementFacetSupport;
 
-public class ApplicationComponent extends WebSphereServiceComponent<WebSphereServerComponent> {
+public class ApplicationComponent extends WebSphereServiceComponent<WebSphereServerComponent> implements MeasurementFacet {
     private ObjectName pattern;
     private MBeanClient mbean;
+    private MeasurementFacetSupport measurementFacetSupport;
     
     @Override
     protected void start() {
@@ -23,6 +31,8 @@ public class ApplicationComponent extends WebSphereServiceComponent<WebSphereSer
         pattern = Utils.createObjectName("WebSphere:type=Application,name=" + context.getResourceKey() + ",*");
         mbean = server.getMBeanClient(pattern);
         server.registerStateChangeEventContext(pattern, context.getEventContext());
+        measurementFacetSupport = new MeasurementFacetSupport(this);
+        measurementFacetSupport.setDefaultHandler(new JMXAttributeGroupHandler(mbean));
     }
     
     public String getApplicationName() {
@@ -48,6 +58,10 @@ public class ApplicationComponent extends WebSphereServiceComponent<WebSphereSer
         } catch (Exception ex) {
             return AvailabilityType.DOWN;
         }
+    }
+
+    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
+        measurementFacetSupport.getValues(report, requests);
     }
 
     public void stop() {
