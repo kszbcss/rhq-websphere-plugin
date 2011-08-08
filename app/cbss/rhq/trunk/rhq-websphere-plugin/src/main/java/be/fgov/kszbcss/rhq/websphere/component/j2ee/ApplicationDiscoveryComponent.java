60a9f1e07f79acb5feb506170e768dc00b9b0ac5
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-08-08 14:11:39 +0000

    Improved logging.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationDiscoveryComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationDiscoveryComponent.java
@@ -1,8 +1,11 @@
 package be.fgov.kszbcss.rhq.websphere.component.j2ee;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rhq.core.pluginapi.inventory.DiscoveredResourceDetails;
 import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
 import org.rhq.core.pluginapi.inventory.ResourceDiscoveryComponent;
@@ -12,10 +15,16 @@ import be.fgov.kszbcss.rhq.websphere.ManagedServer;
 import be.fgov.kszbcss.rhq.websphere.component.server.WebSphereServerComponent;
 
 public class ApplicationDiscoveryComponent implements ResourceDiscoveryComponent<WebSphereServerComponent> {
+    private static final Log log = LogFactory.getLog(ApplicationDiscoveryComponent.class);
+    
     public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext<WebSphereServerComponent> context) throws InvalidPluginConfigurationException, Exception {
         Set<DiscoveredResourceDetails> result = new HashSet<DiscoveredResourceDetails>();
         ManagedServer server = context.getParentResourceComponent().getServer();
-        for (String applicationName : server.queryConfig(new DeployedApplicationsQuery(server.getNode(), server.getServer()))) {
+        String[] applicationNames = server.queryConfig(new DeployedApplicationsQuery(server.getNode(), server.getServer()));
+        if (log.isDebugEnabled()) {
+            log.debug("Discovered the following applications on " + context.getParentResourceComponent().getResourceContext().getResourceKey() + ": " + Arrays.asList(applicationNames));
+        }
+        for (String applicationName : applicationNames) {
             result.add(new DiscoveredResourceDetails(context.getResourceType(), applicationName, applicationName, null, "An enterprise application.", null, null));
         }
         return result;
