c8a9a796a7b4d13c7e897997fd8b3582ad3b63ad
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-08-08 13:21:20 +0000

    Added support for start/stop operations on applications.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationComponent.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/j2ee/ApplicationComponent.java
@@ -4,25 +4,30 @@ import java.util.Set;
 
 import javax.management.ObjectName;
 
+import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementReport;
 import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
 import org.rhq.core.pluginapi.event.EventContext;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
 import org.rhq.core.pluginapi.measurement.MeasurementFacet;
+import org.rhq.core.pluginapi.operation.OperationFacet;
+import org.rhq.core.pluginapi.operation.OperationResult;
 
 import be.fgov.kszbcss.rhq.websphere.ManagedServer;
 import be.fgov.kszbcss.rhq.websphere.Utils;
 import be.fgov.kszbcss.rhq.websphere.component.WebSphereServiceComponent;
 import be.fgov.kszbcss.rhq.websphere.component.server.WebSphereServerComponent;
 import be.fgov.kszbcss.rhq.websphere.mbean.MBeanClient;
+import be.fgov.kszbcss.rhq.websphere.proxy.ApplicationManager;
 import be.fgov.kszbcss.rhq.websphere.support.measurement.JMXAttributeGroupHandler;
 import be.fgov.kszbcss.rhq.websphere.support.measurement.MeasurementFacetSupport;
 
-public class ApplicationComponent extends WebSphereServiceComponent<WebSphereServerComponent> implements MeasurementFacet {
+public class ApplicationComponent extends WebSphereServiceComponent<WebSphereServerComponent> implements MeasurementFacet, OperationFacet {
     private ObjectName pattern;
     private MBeanClient mbean;
     private MeasurementFacetSupport measurementFacetSupport;
+    private ApplicationManager applicationManager;
     
     @Override
     protected void start() {
@@ -33,6 +38,7 @@ public class ApplicationComponent extends WebSphereServiceComponent<WebSphereSer
         server.registerStateChangeEventContext(pattern, context.getEventContext());
         measurementFacetSupport = new MeasurementFacetSupport(this);
         measurementFacetSupport.setDefaultHandler(new JMXAttributeGroupHandler(mbean));
+        applicationManager = server.getMBeanClient("WebSphere:type=ApplicationManager,*").getProxy(ApplicationManager.class);
     }
     
     public String getApplicationName() {
@@ -62,6 +68,15 @@ public class ApplicationComponent extends WebSphereServiceComponent<WebSphereSer
 
     public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
         measurementFacetSupport.getValues(report, requests);
+    }
+
+    public OperationResult invokeOperation(String name, Configuration parameters) throws InterruptedException, Exception {
+        if (name.equals("start")) {
+            applicationManager.startApplication(getApplicationName());
+        } else if (name.equals("stop")) {
+            applicationManager.stopApplication(getApplicationName());
+        }
+        return null;
     }
 
     public void stop() {
