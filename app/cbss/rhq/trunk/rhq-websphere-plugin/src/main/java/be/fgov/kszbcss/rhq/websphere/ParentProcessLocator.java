c438f93e5e0f0a348a08042ee9ddc01c4d87b0e2
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2011-10-21 08:48:51 +0000

    #7892: Added a null check for the result of Discovery#getParent() to avoid NPE if a node agent is down.

--- app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/ParentProcessLocator.java
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/ParentProcessLocator.java
@@ -8,6 +8,7 @@ import be.fgov.kszbcss.rhq.websphere.proxy.Discovery;
 
 import com.ibm.websphere.management.AdminClient;
 import com.ibm.websphere.management.exception.ConnectorException;
+import com.ibm.ws.management.discovery.ServerInfo;
 
 public class ParentProcessLocator implements ProcessLocator {
     private WebSphereServer server;
@@ -19,7 +20,11 @@ public class ParentProcessLocator implements ProcessLocator {
     public void getAdminClientProperties(Properties properties) throws JMException, ConnectorException {
         server.getProcessLocator().getAdminClientProperties(properties);
         String connectorType = properties.getProperty(AdminClient.CONNECTOR_TYPE);
-        for (Properties connector : server.getMBeanClient("WebSphere:type=Discovery,*").getProxy(Discovery.class).getParent().getConnectors()) {
+        ServerInfo parent = server.getMBeanClient("WebSphere:type=Discovery,*").getProxy(Discovery.class).getParent();
+        if (parent == null) {
+            throw new JMException("Parent process not available");
+        }
+        for (Properties connector : parent.getConnectors()) {
             if (connector.getProperty(AdminClient.CONNECTOR_TYPE).equals(connectorType)) {
                 properties.setProperty(AdminClient.CONNECTOR_HOST, connector.getProperty(AdminClient.CONNECTOR_HOST));
                 properties.setProperty(AdminClient.CONNECTOR_PORT, connector.getProperty(AdminClient.CONNECTOR_PORT));
