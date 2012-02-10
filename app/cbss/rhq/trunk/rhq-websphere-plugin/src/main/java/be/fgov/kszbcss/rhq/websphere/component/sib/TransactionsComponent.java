40ffc8c6bb543d0c1b6bbfb9fdbd498d875e9cef
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2012-02-10 14:32:54 +0000

    Added support for SIBus transaction statistics.

---
+++ app/cbss/rhq/trunk/rhq-websphere-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/component/sib/TransactionsComponent.java
@@ -0,0 +1,46 @@
+package be.fgov.kszbcss.rhq.websphere.component.sib;
+
+import java.util.Set;
+
+import org.rhq.core.domain.measurement.AvailabilityType;
+import org.rhq.core.domain.measurement.MeasurementReport;
+import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
+import org.rhq.core.pluginapi.inventory.InvalidPluginConfigurationException;
+import org.rhq.core.pluginapi.measurement.MeasurementFacet;
+
+import be.fgov.kszbcss.rhq.websphere.ManagedServer;
+import be.fgov.kszbcss.rhq.websphere.component.WebSphereServiceComponent;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.MeasurementFacetSupport;
+import be.fgov.kszbcss.rhq.websphere.support.measurement.PMIMeasurementHandler;
+
+public class TransactionsComponent extends WebSphereServiceComponent<SIBMessagingEngineComponent> implements MeasurementFacet {
+    private MeasurementFacetSupport measurementFacetSupport;
+    
+    @Override
+    protected void start() throws InvalidPluginConfigurationException, Exception {
+        ManagedServer server = getServer();
+        measurementFacetSupport = new MeasurementFacetSupport(this);
+        measurementFacetSupport.addHandler("stats", new PMIMeasurementHandler(server.getServerMBean(),
+                "SIB Service", "SIB Messaging Engines", getResourceContext().getParentResourceComponent().getName(),
+                "Storage Management", "Transactions"));
+    }
+
+    @Override
+    protected boolean isConfigured() throws Exception {
+        // The SIB messaging engine cache is always configured
+        return true;
+    }
+
+    protected AvailabilityType doGetAvailability() {
+        return AvailabilityType.UP;
+    }
+
+    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> requests) throws Exception {
+        if (getResourceContext().getParentResourceComponent().isActive()) {
+            measurementFacetSupport.getValues(report, requests);
+        }
+    }
+
+    public void stop() {
+    }
+}
