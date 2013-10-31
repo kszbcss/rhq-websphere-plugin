be8fb965894d41618e01056ce1c3fafb4ed6f066
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2013-10-31 12:55:03 +0000

    More debug logging to investigate issue #5.

--- rhq-websphere-agent-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/config/ConfigObject.java
+++ rhq-websphere-agent-plugin/src/main/java/be/fgov/kszbcss/rhq/websphere/config/ConfigObject.java
@@ -29,6 +29,9 @@ import javax.management.AttributeList;
 import javax.management.JMException;
 import javax.management.ObjectName;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import be.fgov.kszbcss.rhq.websphere.proxy.AppManagement;
 import be.fgov.kszbcss.rhq.websphere.proxy.ConfigService;
 
@@ -42,6 +45,8 @@ import com.ibm.websphere.management.exception.ConnectorException;
  * than {@link ConfigService}.
  */
 public class ConfigObject {
+    private static final Log log = LogFactory.getLog(ConfigObject.class);
+    
     private final CellConfiguration config;
     private final ObjectName objectName;
     private AttributeList attributes;
@@ -61,11 +66,17 @@ public class ConfigObject {
     
     private AttributeList getAttributes() throws JMException, ConnectorException, InterruptedException {
         if (attributes == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Loading attributes for configuration object " + objectName + " ...");
+            }
             attributes = config.execute(new SessionAction<AttributeList>() {
                 public AttributeList execute(ConfigService configService, AppManagement appManagement, Session session) throws JMException, ConnectorException {
                     return configService.getAttributes(session, objectName, null, false);
                 }
             });
+            if (log.isDebugEnabled()) {
+                log.debug("Result: " + attributes);
+            }
         }
         return attributes;
     }
