d4c8ece8906c0d0f6d6ae45e49fca02fb7baa81d
andreas.veithen <andreas.veithen@25399334-bce1-c7a2-fc85-eb4e137d2a91>
2014-01-09 15:15:41 +0000

    Simplify the annotation processor. Since the annotation is part of the same artifact, we can use it directly (instead of the annotation mirrors).

--- annotation-processor/src/main/java/be/fgov/kszbcss/rhq/websphere/config/ConfigObjectTypeAnnotationProcessor.java
+++ annotation-processor/src/main/java/be/fgov/kszbcss/rhq/websphere/config/ConfigObjectTypeAnnotationProcessor.java
@@ -1,6 +1,6 @@
 /*
  * RHQ WebSphere Plug-in
- * Copyright (C) 2013 Crossroads Bank for Social Security
+ * Copyright (C) 2013-2014 Crossroads Bank for Social Security
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -24,7 +24,6 @@ package be.fgov.kszbcss.rhq.websphere.config;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -33,10 +32,7 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.StandardLocation;
 
@@ -48,22 +44,9 @@ public class ConfigObjectTypeAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (!annotations.isEmpty()) {
-            // Since we support a single annotation, the set is a singleton containing only
-            // the ConfigObjectType annotation type
-            TypeElement annotationType = annotations.iterator().next();
-            for (Element element : roundEnv.getElementsAnnotatedWith(annotationType)) {
+            for (Element element : roundEnv.getElementsAnnotatedWith(ConfigObjectType.class)) {
                 TypeElement type = (TypeElement)element;
-                for (AnnotationMirror annotation : type.getAnnotationMirrors()) {
-                    if (annotation.getAnnotationType().asElement() == annotationType) {
-                        String name = null;
-                        for (Map.Entry<? extends ExecutableElement,? extends AnnotationValue> entry : annotation.getElementValues().entrySet()) {
-                            if (entry.getKey().getSimpleName().contentEquals("name")) {
-                                name = (String)entry.getValue().getValue();
-                            }
-                        }
-                        mappings.setProperty(name, type.getQualifiedName().toString());
-                    }
-                }
+                mappings.setProperty(type.getAnnotation(ConfigObjectType.class).name(), type.getQualifiedName().toString());
             }
         }
         if (roundEnv.processingOver()) {
